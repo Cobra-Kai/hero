495fa3891bd5dc8da6d8f0334129ff4e46c403e1
Jon Mayo <jon@rm-f.net>
2015-03-14 22:13:01 -0700

    hero: add command-line options
    Add command-line options -geometry and -vsync and -verbose
    use SDL_Log instead of fprintf

--- hero.c
+++ hero.c
@@ -1,7 +1,6 @@
 /*
  * Copyright © 2015 Jon Mayo <jon@rm-f.net>
  */
-#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdbool.h>
@@ -10,11 +9,19 @@
 
 struct config {
 	int width, height;
+	bool verbose; /* enable to turn on extra logging */
+	bool use_vsync;
 };
 
-/* default configuration */
+struct game_state {
+	GLuint win_x, win_y, win_w, win_h; /* used to crop to preserve aspect */
+};
+
+/* global configuration */
 static struct config config = {
-	.width = 640, .height = 480,
+	.width = 960, .height = 540,
+	.verbose = true,
+	.use_vsync = false,
 };
 
 static bool keep_going = true;
@@ -24,7 +31,8 @@ static void die(const char *fmt, ...)
 {
 	va_list ap;
 	va_start(ap, fmt);
-	vfprintf(stderr, fmt, ap);
+	SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
+		fmt, ap);
 	va_end(ap);
 	exit(EXIT_FAILURE);
 }
@@ -33,35 +41,93 @@ static void warn(const char *fmt, ...)
 {
 	va_list ap;
 	va_start(ap, fmt);
-	fprintf(stderr, "Warning: ");
-	vfprintf(stderr, fmt, ap);
+	SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_WARN,
+		fmt, ap);
 	va_end(ap);
 }
 
 static void setup_gl(void)
 {
 	SDL_Window *win = SDL_GL_GetCurrentWindow();
-	if (SDL_GL_SetSwapInterval(1) < 0)
-		warn("unable to configure VSync\n");
 
-	glClearColor(1.0, 1.0, 0.0, 1.0);
+	/* setup the config.use_vsync option */
+	if (SDL_GL_GetSwapInterval() != -1) {
+		if (SDL_GL_SetSwapInterval(config.use_vsync ? 1 : 0) < 0)
+			warn("unable to configure VSync\n");
+	} else {
+		warn("Configuring VSync is not supported!\n");
+	}
+
+	glClearColor(1.0, 0.75, 0.0, 1.0);
 }
 
-static void render(void)
+static void game_paint(void)
 {
 	SDL_Window *win = SDL_GL_GetCurrentWindow();
+	struct game_state *state = SDL_GetWindowData(win, "game");
+	SDL_assert(state != NULL);
+
+	glScissor(state->win_x, state->win_y, state->win_w, state->win_h);
 	glClear(GL_COLOR_BUFFER_BIT);
 }
 
-int main()
+static void usage(const char *argv0)
 {
+	// TODO: should we replace this with SDL_Log() ?
+	fprintf(stderr, "%s [-geometry %dx%d]\n",
+		argv0, config.width, config.height);
+	exit(EXIT_FAILURE);
+}
+
+static void parse_args(int argc, char **argv)
+{
+	int i;
+
+	for (i = 1; i < argc; ) {
+		const char *cur = argv[i++];
+
+		if (!strcmp(cur, "-help") || !strcmp(cur, "-h")) {
+			usage(argv[0]);
+		} else if (!strcmp(cur, "-geometry") || !strcmp(cur, "-geom")) {
+			if (i >= argc) {
+				fprintf(stderr, "ERROR at %s\n", cur);
+				usage(argv[0]);
+			}
+			const char *arg = argv[i++];
+			if (sscanf(arg, "%dx%d", &config.width, &config.height) != 2) {
+				fprintf(stderr, "ERROR at %s\n", cur);
+				usage(argv[0]);
+			}
+		} else if (!strcmp(cur, "-verbose")) {
+			config.verbose = true;
+		} else if (!strcmp(cur, "-vsync")) {
+			config.use_vsync = true;
+		} else if (!strcmp(cur, "-novsync") ||
+			!strcmp(cur, "-no-vsync")) {
+			config.use_vsync = false;
+		} else {
+			fprintf(stderr, "ERROR unknown option %s\n", cur);
+			usage(argv[0]);
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+	parse_args(argc, argv);
+
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER) < 0)
 		die("SDL Could not initialize! (%s)\n", SDL_GetError());
 
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 
-	SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
+	if (config.verbose) {
+		SDL_LogSetAllPriority(SDL_LOG_PRIORITY_WARN);
+	} else {
+		SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION,
+			SDL_LOG_PRIORITY_INFO);
+	}
 
 	SDL_Window *main_window = SDL_CreateWindow("Hero",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -70,19 +136,20 @@ int main()
 	if (!main_window)
 		die("SDL Could not create window! (%s)\n", SDL_GetError());
 
-#if 0
-	/* If we're not using GL ... */
-	if (0) {
-	SDL_Surface *main_surface = SDL_GetWindowSurface(main_window);
-	if (!main_surface)
-		die("SDL could not get surface buffer! (%s)\n", SDL_GetError());
-	}
-#endif
+	/* create the game related data and attach it to a window */
+	struct game_state *main_state = calloc(1, sizeof(*main_state));
+	int width, height;
+	SDL_GetWindowSize(main_window, &width, &height);
+	main_state->win_x = 0;
+	main_state->win_y = 0;
+	main_state->win_w = width;
+	main_state->win_h = height;
+	SDL_SetWindowData(main_window, "game", main_state);
 
+	/** GL related setup **/
 	SDL_GLContext main_context = SDL_GL_CreateContext(main_window);
 	if (!main_context)
 		die("SDL could not create GL context! (%s)\n", SDL_GetError());
-
 	setup_gl();
 
 	/*** Controller ***/
@@ -98,14 +165,14 @@ int main()
 				break;
 			case SDL_TEXTINPUT:
 				// TODO: do something with this event
-				fprintf(stderr, "Info: text=%.32s\n", ev.text.text);
+				SDL_Log("ext=%.32s\n", ev.text.text);
 				break;
 			}
 		}
 
 		/* Render/Paint */
 		SDL_GL_MakeCurrent(main_window, main_context); /* not needed for single window applications */
-		render();
+		game_paint();
 		SDL_GL_SwapWindow(main_window);
 	}
 
